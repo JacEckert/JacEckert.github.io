628670e0d4eb21442448481463320dab831a3d80
JacEckert <JacEckert@gmail.com>
2018-06-07 19:27:22 -0500

    code cleanup
    Moved Reset Data button into only the outcome section
    
    Added or tweaked comments and improved readability

--- scripts.js
+++ scripts.js
@@ -1,4 +1,13 @@
-/* 
+////////////////////
+//    Scramble    //
+//      Race      //
+////////////////////
+
+
+////////////////////////
+// Database Structure //
+////////////////////////
+/*
 {
 	"gameData": {
 		"startTime": 0,
@@ -22,19 +31,23 @@ var letterBank;
 ////////////////
 // Timer Data //
 ////////////////
-var time = 20;							//temporary decrease for faster testing
+var time = 60;
 var timerInterval;
 var wordInterval;
 var endInterval;
-
-var wordDone = false;
-var wordResult;    //temporary hopefully
 
 /////////////////
 // Player Data //
 /////////////////
 var player;
 var playerScore = 0;
+
+///////////////
+// Word Data //
+///////////////
+var wordDone;
+var wordIsNew;
+
 
 //checks for existing user, displays error if exists
 	//or populates user display and removes name input
@@ -69,10 +82,6 @@ function startGame() {
 	toggle("start");
 	toggle("game");
 	
-	//time given in milliseconds, divide by 1000 to translate to seconds
-		//and floor to remove insignificant digits
-//database: check if game data exists
-	
 	gameRef = database.ref('gameData/');
 	var now = new Date().getTime();
 	
@@ -81,6 +90,8 @@ function startGame() {
 		//if game has already started, retrieve data
 			//else set default time and generate letter bank
 		if(snapshot.val()){
+			//date time given in milliseconds, divide the difference of now and the
+				//start time by 1000 to convert to seconds for interval tracking
 			time = Math.floor(time-((now - snapshot.child("startTime").val())/1000));
 			letterBank = snapshot.child("letterBank").val();
 		} else {
@@ -90,7 +101,6 @@ function startGame() {
 		document.getElementById("letterBank").innerHTML = letterBank;
 		timerInterval = setInterval("gameTimer()", 1000);
 	});
-
 }
 
 //generates a string of random characters with at least 2 vowels and 2 consonants
@@ -114,7 +124,7 @@ function generateBank() {
 	return text;
 }
 
-//updates the remaining time and ends the game at 0
+//updates the remaining time and ends the game after displaying time = 0
 function gameTimer() {
 	document.getElementById("timer").innerHTML = time--;
 	if(time === -2) {
@@ -126,6 +136,7 @@ function gameTimer() {
 //hides game, updates user data in database, gets user data, and displays outcome
 function endGame() {
 	toggle("gameContainer");
+	
 	database.ref('players/' + player).set({score : playerScore, isDone : true});
 	endInterval = setInterval("getResults()", 250);
 	
@@ -136,13 +147,13 @@ function endGame() {
 function submitWord() {
 	wordDone = false;
 	var word = document.getElementById("word").value.toUpperCase();
-	var one = isValid(word);
-	var two = isNew(word);
+	isNew(word);
+	//attempts to verify word until wordDone returns as true
+		//this signals that wordIsNew has been updated within isNew to be accurate
 	wordInterval = setInterval(function() {
 		if(wordDone) {
 			clearInterval(wordInterval);
-			if(one && wordResult) {
-				
+			if(isValid(word) && wordIsNew) {
 				playerScore += word.length;
 				document.getElementById("score").innerHTML = playerScore;
 				database.ref('words/').push().set(word);
@@ -155,20 +166,23 @@ function submitWord() {
 
 //checks database and returns whether the word already exists or not
 function isNew(word) {
-	wordResult = true;
-
+	wordIsNew = true;
 	var wordRef = database.ref('words/');
 	
 	wordRef.once("value", snapshot => {
-		//if words list exists, check for submitted word, return false
+		//if words list exists, check for submitted word
+			//else list is empty, word clearly not used
 		if(snapshot.val()){
 			for(var key in snapshot.val()) {
 				var newWord = snapshot.val()[key];
+				//if any newWord in the list matches the submitted word, result is false
+					//else, default result is true
 				if(word == newWord) {
-					wordResult = false;
+					wordIsNew = false;
 				}
 			}
 		}
+		//marks word check as complete
 		wordDone = true;
 	});
 }
@@ -194,7 +208,9 @@ function getResults() {
 	database.ref('players/').once("value", snapshot => {
 		var max = 0;
 		var maxCount = "";
-			
+		
+		//checks that each user has submitted their final score
+			//holds the name and score of whoever has the highest score
 		for(var user in snapshot.val()) {
 			if(!snapshot.val()[user].isDone) {
 				return;
@@ -206,6 +222,7 @@ function getResults() {
 			}
 		}
 		clearInterval(endInterval);
+		
 		document.getElementById("winScore").innerHTML += max;
 		document.getElementById("playerScore").innerHTML += playerScore;
 		
@@ -215,6 +232,13 @@ function getResults() {
 			announce(maxCount + " Wins!");
 		}
 	});
+}
+
+//removes all content from the database
+function clearData(){
+	database.ref('gameData' ).remove();
+	database.ref('players' ).remove();
+	database.ref('words' ).remove();
 }
 
 //temporarily shows whatever phrase is passed
@@ -232,10 +256,4 @@ function toggle(ID) {
 		e.style.display = "block";
 	else
 		e.style.display = "none";
-}
-
-function clearData(){
-	database.ref('gameData' ).remove();
-	database.ref('players' ).remove();
-	database.ref('words' ).remove();
 }
